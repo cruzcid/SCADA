ba49e2019cbd76cb809fe1e7b2b4114f66df04a2
cruzcid <abstractime@outlook.com>
2015-11-21 14:26:23 -0600

    Agregue POST a interface ScadaApiService.java asi como constantes de busqueda de url

--- app/src/main/java/com/example/cruzjedi/tesisprojectscada/io/model/ScadaApiService.java
+++ app/src/main/java/com/example/cruzjedi/tesisprojectscada/io/model/ScadaApiService.java
@@ -1,7 +1,10 @@
 package com.example.cruzjedi.tesisprojectscada.io.model;
 
 import retrofit.Callback;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
 import retrofit.http.GET;
+import retrofit.http.POST;
 
 /**
  * Created by cruzjedi on 17/11/15.
@@ -9,4 +12,8 @@ import retrofit.http.GET;
 public interface ScadaApiService {
     @GET(ApiConstants.PETITION)
     void getScadaDatosSalon(Callback<ScadaDatosSalonResponse> serverResponse);
+
+    @FormUrlEncoded
+    @POST(ApiConstants.ENVIAR_SALON_PARAM)
+    void getSalonDatos(@Field("salon")String  salonDato, Callback<ScadaDatosSalonResponse> cb);
 }
