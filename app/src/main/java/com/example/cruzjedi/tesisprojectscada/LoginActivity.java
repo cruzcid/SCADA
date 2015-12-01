3e0097953835df0cb37d906cc370425714b4e500
Cruz Cast <abstractime@outlook.com>
2015-12-01 00:38:34 -0600

    LogIn Desin finished. Edit text flotantes. Falta agregar backend al php

--- app/src/main/java/com/example/cruzjedi/tesisprojectscada/LoginActivity.java
+++ app/src/main/java/com/example/cruzjedi/tesisprojectscada/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.example.cruzjedi.tesisprojectscada;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -11,17 +13,23 @@ import android.widget.EditText;
  */
 public class LoginActivity extends AppCompatActivity{
 
-    private Button logUser;
+    private Button loginUser;
     private EditText user, password;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);/*
-        logUser = (Button) findViewById(R.i.btn_log_user);
-        user = (EditText) findViewById(R.id.edtxt_password);
-        password = (EditText) findViewById(R.id.edtxt_password);*/
+        setContentView(R.layout.activity_login);
+        loginUser = (Button) findViewById(R.id.btn_login_user);
+        user = (EditText) findViewById(R.id.edtxt_user);
+        password = (EditText) findViewById(R.id.edtxt_password);
 
+        loginUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-
 }
